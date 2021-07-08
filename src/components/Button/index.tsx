9b09df2cd625b87c93d58ea9ccc3f5113db1d182
ElandVarse <andre.jobs23@gmail.com>
2021-07-08 11:22:49 -0300

    File structure and beta visual

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import './styles.scss'
+
+function Button() {
+    return (
+        <div className="btn">
+            botao
+        </div>
+    )
+}
+
+export default Button
---
+++ src/components/Fliper/index.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+import './styles.scss';
+
+import Button from '../Button/index';
+
+function Fliper() {
+    return (
+        <div className="screenBorders">
+            <div className="screen">
+
+            </div>
+
+            <div className="btnContainer">
+                {/* <Button />
+                <Button />
+                <Button /> */}
+            </div>
+        </div>
+    )
+}
+
+export default Fliper
